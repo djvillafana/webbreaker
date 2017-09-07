c64a809110010bbe38d7e002eae9a5309a91dc77
Matthew.Dunaj <z002r0t@dca9046e8d47.hq.target.com>
2017-09-07 11:23:31 -0500

    Refactor setup Fernet usage, add try/finally

--- setup.py
+++ setup.py
@@ -48,23 +48,19 @@ class PyTest(TestCommand):
         err = pytest.main(shlex.split(self.pytest_args))
         sys.exit(err)
 
-
-if sys.argv[-1] == 'secret':
+def set_secret():
     key = Fernet.generate_key()
     with open(".webbreaker", 'w') as secret_file:
         secret_file.write(key.decode())
     os.chmod('.webbreaker', 0o400)
     print("New secret has been set.")
+
+if sys.argv[-1] == 'secret':
+    set_secret()
     sys.exit(0)
 
 # build and install helper
 if sys.argv[-1] == 'install':
-    if not os.path.isfile('.webbreaker'):
-        key = Fernet.generate_key()
-        with open(".webbreaker", 'w') as secret_file:
-            secret_file.write(key.decode())
-        os.chmod('.webbreaker', 0o400)
-
     os.system('python setup.py install --user')
     sys.exit(0)
 
@@ -72,46 +68,48 @@ if sys.argv[-1] == 'build':
     os.system('python setup.py sdist bdist_wheel')
     sys.exit(0)
 
-setup(
-    name='webbreaker',
-    description='Application to automate Dynamic Application Security Test Orchestration (DASTO).',
-    long_description=open('README.md').read(),
-    version=version,
-    author='Brandon Spruth, Jim Nelson, Matthew Dunaj',
-    author_email='brandon.spruth2@target.com, jim.nelson2@target.com, matthew.dunaj@target.com',
-    license='MIT',
-    url="https://github.com/target/webbreaker",
-    packages=find_packages(exclude=['docs', 'tests']),
-    include_package_data=True,
-    zip_safe=False,
-    py_modules=['webbreaker.webbreakercli'],
-    package_data={'configs': ['webbreaker/etc/logging.ini',
-                              'webbreaker/etc/fortify.ini',
-                              'webbreaker/etc/email.ini',
-                              'webbreaker/etc/webinspect.ini',
-                              'images/WebBreakerArchitecture.jpg']
-    },
-    install_requires=requires,
-    entry_points='''
-        [console_scripts]
-        webbreaker=webbreaker.webbreakercli:cli
-    ''',
-    classifiers=[
-        'Programming Language :: Python',
-        'Development Status :: 4 - Beta',
-        'Environment :: Other Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation ::CPython'],
-    extras_require={'test': tests_require},
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest}
-)
-
+try:
+    setup(
+        name='webbreaker',
+        description='Application to automate Dynamic Application Security Test Orchestration (DASTO).',
+        long_description=open('README.md').read(),
+        version=version,
+        author='Brandon Spruth, Jim Nelson, Matthew Dunaj',
+        author_email='brandon.spruth2@target.com, jim.nelson2@target.com, matthew.dunaj@target.com',
+        license='MIT',
+        url="https://github.com/target/webbreaker",
+        packages=find_packages(exclude=['docs', 'tests']),
+        include_package_data=True,
+        zip_safe=False,
+        py_modules=['webbreaker.webbreakercli'],
+        package_data={'configs': ['webbreaker/etc/logging.ini',
+                                  'webbreaker/etc/fortify.ini',
+                                  'webbreaker/etc/email.ini',
+                                  'webbreaker/etc/webinspect.ini',
+                                  'images/WebBreakerArchitecture.jpg']
+        },
+        install_requires=requires,
+        entry_points='''
+            [console_scripts]
+            webbreaker=webbreaker.webbreakercli:cli
+        ''',
+        classifiers=[
+            'Programming Language :: Python',
+            'Development Status :: 4 - Beta',
+            'Environment :: Other Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Software Development :: Libraries :: Application Frameworks',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.0',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: Implementation ::CPython'],
+        extras_require={'test': tests_require},
+        tests_require=['pytest'],
+        cmdclass={'test': PyTest}
+    )
+finally:
+    set_secret()
